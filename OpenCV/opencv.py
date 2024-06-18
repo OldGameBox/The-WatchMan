17425e163980374bbfc2c88baf1d0741a1bf9847
NeonValstrax <maddoxfeemster@gmail.com>
2024-06-18 15:11:48 -0400

    added if statement

--- OpenCV/opencv.py
+++ OpenCV/opencv.py
@@ -1,6 +1,14 @@
 import cv2
 
-img = cv2.imread('image.jpg',1)
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cap = cv2.VideoCapture(0)
+
+ret, frame = cap.read()
+
+if ret:
+
+    cv2.imshow("GeeksForGeeks", frame)
+    cv2.imwrite('captured_image.jpg', frame)
+
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
