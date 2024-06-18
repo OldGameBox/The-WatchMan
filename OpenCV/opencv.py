74d704cfa85f15e2d72cc54e851c108a22c86215
OldGameBox <mr.kumthebest@gmail.com>
2024-06-18 15:55:45 -0400

    Merged opencv.py and jaycv2.py together. Reformated opencv.py. Added __name__ == "__main__" to TTS

--- OpenCV/opencv.py
+++ OpenCV/opencv.py
@@ -1,14 +1,40 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0)
 
-ret, frame = cap.read()
 
-if ret:
+def start_camera():
+    while True:
+        # Capture the video frame
+        # by frame
+        ret, frame = camera.read()
 
-    cv2.imshow("GeeksForGeeks", frame)
-    cv2.imwrite('captured_image.jpg', frame)
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
 
-    cv2.waitKey(0)
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
+    # After the loop release the cap object
+    camera.release()
+    # Destroy all the windows
     cv2.destroyAllWindows()
+
+
+def get_image_from_camera():
+    ret, frame = camera.read()
+
+    if ret:
+        cv2.imshow("GeeksForGeeks", frame)
+        cv2.imwrite('captured_image.jpg', frame)
+
+        cv2.waitKey(0)
+
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    pass
--- tts/TTS.py
+++ tts/TTS.py
@@ -1,10 +1,12 @@
 import pyttsx3
 
+
 def text_to_speech(text):
-     speech = input(text)
-     engine = pyttsx3.init()
-     engine.say(speech)
-     engine.runAndWait()
+    speech = input(text)
+    engine = pyttsx3.init()
+    engine.say(speech)
+    engine.runAndWait()
+
 
 if __name__ == "__main__":
     print("hi")
