4a111779401e067c7841840553ca67323015b8c5
OldGameBox <mr.kumthebest@gmail.com>
2024-06-18 15:42:12 -0400

    Updated tts function

--- tts/TTS.py
+++ tts/TTS.py
@@ -2,8 +2,8 @@ import pyttsx3
 
 
 while 0 == 0:
-    def text_to_speech():
-         speech = input("What do you want to be said?: ")
+    def text_to_speech(text):
+         speech = input(text)
          engine = pyttsx3.init()
          engine.say(speech)
          engine.runAndWait()
