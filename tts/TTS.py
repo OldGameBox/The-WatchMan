6da0312f7498590668401392c259c1f6a2e121e9
Jayd3ee <jayd3e.st@gmail.com>
2024-06-18 15:48:48 -0400

    Updated tts

--- tts/TTS.py
+++ tts/TTS.py
@@ -1,11 +1,10 @@
 import pyttsx3
 
+def text_to_speech(text):
+     speech = input(text)
+     engine = pyttsx3.init()
+     engine.say(speech)
+     engine.runAndWait()
 
-while 0 == 0:
-    def text_to_speech(text):
-         speech = input(text)
-         engine = pyttsx3.init()
-         engine.say(speech)
-         engine.runAndWait()
-
-    text_to_speech()
+if __name__ == "__main__":
+    print("hi")
