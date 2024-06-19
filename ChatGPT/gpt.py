29e11013b734982b3d57cba3ab31e509b0ffa749
OldGameBox <mr.kumthebest@gmail.com>
2024-06-19 14:40:02 -0400

    Updated libraries and main.py

--- ChatGPT/gpt.py
+++ ChatGPT/gpt.py
@@ -1,57 +1,57 @@
 import cv2
 import base64
 import requests
-import pyttsx3
 
-camera = cv2.VideoCapture(0)
-
-api_key = open('../openai.api', 'r').read()
-
-# ret, image = camera.read()
-# cv2.imwrite('captured_image.jpg', image)
-
+api_key = open('openai.api', 'r').read()
 headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {api_key}"
 }
 
-def text_to_speech(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
-    
+
+def scan(camera: cv2.VideoCapture, debug: bool = False) -> str:
+    _, image = camera.read()
+    cv2.imwrite('captured_image.jpg', image)
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=generate_payload())
+    if debug: print("Scan Result:" + response.json()['choices'][0]['message']['content'])
+    return response.json()['choices'][0]['message']['content']
+
+
+def generate_payload() -> dict:
+    return {
+        "model": "gpt-4o",
+        "messages": [
+            {
+                "role": "system",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "You will receive images of different objects. You must identify and describe ONLY THE MAIN OBJECT in the middle of image. Use ONLY around 2-3 sentences. If you cannot identify an object, you MUST ONLY RESPOND: 'IDENT_ERROR'. Start by saying: 'I see...'"
+                    }
+                ]
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{encode_image('captured_image.jpg')}"
+                        }
+                    }
+                ]
+            }
+        ],
+        "max_tokens": 300
+    }
+
+
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 
-payload = {
-    "model": "gpt-4o",
-    "messages": [
-        {
-            "role": "system",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "You will receive images of different objects. You must identify and describe ONLY THE MAIN OBJECT in the middle of image in details. If you cannot identify an object, you MUST ONLY SAY: 'IDENT_ERROR'. Start by saying: 'I see...'"
-                }
-            ]
-        },
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{encode_image('captured_image.jpg')}"
-                    }
-                }
-            ]
-        }
-    ],
-    "max_tokens": 300
-}
-
-response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-
-text_to_speech(response.json()['choices'][0]['message']['content'])
+if __name__ == "__main__":
+    api_key = open('../openai.api', 'r').read()
+    camera = cv2.VideoCapture(0)
+    scan(camera, True)
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import tts.TTS as tts
-import OpenCV.opencv as cv
+from tts import TTS
+from ChatGPT import gpt
+import cv2 as cv
 
 if __name__ == "__main__":
-    tts.text_to_speech("Greetings, Keeper of Time!")
+    camera = cv.VideoCapture(0)
+    while True:
+        _, image = camera.read()
+        cv.imshow("The WatchMan [Prototype]", image)
+        if cv.waitKey(1) == 32:
+            TTS.text_to_speech(gpt.scan(camera, True))
+        if cv.waitKey(1) == ord('q'):
+            break
+    camera.release()
+    cv.destroyAllWindows()
